571f7ae76006fb1d6a91e23b4759ae4e9adf8d9d
mehadi07 <saimum22@gmail.com>
2017-05-24 16:35:49 -0400

    updates

--- seperate-router-controllers-model-post/controllers/moviesController.js
+++ seperate-router-controllers-model-post/controllers/moviesController.js
@@ -27,9 +27,9 @@ var add = function (req, res) {
     });
 };
 
-var getById = function(req, res){
-    Movie.findById(req.params.id, function(err, movie){
-        if(err){
+var getById = function (req, res) {
+    Movie.findById(req.params.id, function (err, movie) {
+        if (err) {
             res.status(404);
             res.send("Not Found");
         } else {
@@ -37,6 +37,30 @@ var getById = function(req, res){
             res.send(movie);
         }
     })
+};
+
+var update = function (req, res) {
+    Movie.findById(req.params.id, function (err, movie) {
+        if (err) {
+            res.status(404);
+            res.send("Not Found");
+        } else {
+            movie.title = req.body.title;
+            movie.genre = req.body.genre;
+            movie.rating = req.body.rating;
+            movie.isReleased = req.body.isReleased;
+
+            movie.save(function (err) {
+                if (!err) {
+                    res.status(200);
+                    res.send(movie);
+                } else {
+                    res.status(500);
+                    res.send("Failed");
+                }
+            })
+        }
+    });
 };
 
 //module.exports.get = get;
